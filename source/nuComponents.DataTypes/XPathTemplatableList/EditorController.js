4cc930ff39108f19461b4023ae42c452bc9024a1
Hendy Racher <git@hendyracher.co.uk>
2014-04-28 00:00:39 +0100

    XPathTemplatableList - refactored editer controller js to use two arrays of option objects, rather than a single string array of selected keys

--- source/nuComponents.DataTypes/XPathTemplatableList/EditorController.js
+++ source/nuComponents.DataTypes/XPathTemplatableList/EditorController.js
@@ -31,56 +31,76 @@ angular
                 };                
             */
 
-            // local data
-            $scope.selectedKeys = [];
+            // array of option objects, for the selectable list
+            $scope.selectableOptions = []; // [{"key":"","markup":""},{"key":"","markup":""}...]
 
+            // array of option objects, for the selected list
+            $scope.selectedOptions = []; // [{"key":"","markup":""},{"key":"","markup":""}...]
+
+            // call api, supplying all configuration details, and expect a collection of options (key / markup) to be populated
             apiResource.getEditorOptions($scope.model.config).then(function (response) {
 
-                // get data to build ui
-                $scope.editorOptions = response.data; // [{"key":"","markup":""},{"key":"","markup":""}...]
+                var editorOptions = response.data; // [{"key":"","markup":""},{"key":"","markup":""}...]
 
-                // rebuild selectedKeys from csv
-                if ($scope.model.value) {
-                    var savedKeys = $scope.model.value.split(',');
-                    for (var i = 0; i < savedKeys.length; i++) {
-                        // TODO: check to see if this key is in the available editor options
-
-                        $scope.selectedKeys.push(savedKeys[i]);
-
+                // build selected options (from saved csv)
+                var savedKeys = $scope.model.value.split(',');
+                for (var i = 0; i < savedKeys.length; i++) { // loop though each saved key
+                    for (var j = 0; j < editorOptions.length; j++) { // loop though each editor option
+                        if (savedKeys[i] == editorOptions[j].key) {
+                            $scope.selectedOptions.push(editorOptions[j]);
+                            break; // break out of the editor option loop
+                        }
                     }
                 }
 
-                // setup watch on local data
-                $scope.$watch(function () { return $scope.selectedKeys.length; }, function () {
-                    // set the model.value (for Umbraco to persist)
-                    $scope.model.value = $scope.selectedKeys.join();
+                // setup watch on selected options to recreate the csv in model.value for Umbraco
+                $scope.$watch(function () { return $scope.selectedOptions.length; }, function () {
+                    $scope.model.value = $scope.selectedOptions.map(function (option) { return option.key; }).join();
                 });
 
+                // build selectable options
+                for (var i = 0; i < editorOptions.length; i++) {
+                    // always add if duplicates can be selected
+                    if ($scope.model.config.allowDuplicates == '1') { 
+                        $scope.selectableOptions.push(editorOptions[i]);
+                    }
+                    // add if this option isn't already in the selected options array
+                    else if ($scope.selectedOptions.indexOf(editorOptions[i]) == -1) {
+                        $scope.selectableOptions.push(editorOptions[i]);
+                    }
+                }
             });
 
-            $scope.isVisibileOption = function (key) {
-                // TODO: add 'Hide Invalid Options'
-                return $scope.isActiveOption(key);
-            };
-
-            $scope.isActiveOption = function (key) {
-                if ($scope.model.config.allowDuplicates == '1' || $scope.selectedKeys.indexOf(key) == -1) {
+            // return ture, if the option can be selected (this is only relevant when hideInactive = false)
+            $scope.isActive = function (option) {
+                if ($scope.model.config.allowDuplicates == '1' ||
+                    $scope.selectedOptions.indexOf(option) == -1) {
                     return true;
                 }
                 return false;
             };
 
             // picking an item from 'selectable' for 'selected'
-            $scope.selectOption = function (key) {
+            $scope.selectOption = function (option) {
 
                 //TODO: check not exceeding the max
 
                 // add item if allowing duplicates, else check it's not already in the list
                 if ($scope.model.config.allowDuplicates == '1') {
-                    $scope.selectedKeys.push(key);
+                    $scope.selectedOptions.push(option);                    
                 }
-                else if ($scope.selectedKeys.indexOf(key) == -1) {
-                    $scope.selectedKeys.push(key);
-                }                
+                // TODO: does this key already exist in the selectedOptions ?
+                else if ($scope.selectedOptions.indexOf(option) == -1) {
+                    $scope.selectedOptions.push(option);
+
+                    // TODO: config value for hideInactive
+                    // currently assuming hideInactive = true
+                    for (var i = 0; i < $scope.selectableOptions.length; i++) {
+                        if ($scope.selectableOptions[i] == option) {
+                            $scope.selectableOptions.splice(i, 1);
+                            break;
+                        }
+                    }
+                }
             };
     }]);
