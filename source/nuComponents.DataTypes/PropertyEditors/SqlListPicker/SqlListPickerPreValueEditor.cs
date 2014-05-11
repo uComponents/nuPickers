eefe7ee51d2b56f0aaa94fe06d707fd57f478ebd
Hendy Racher <git@hendyracher.co.uk>
2014-05-11 19:25:25 +0100

    added SqlListPicker

---
+++ source/nuComponents.DataTypes/PropertyEditors/SqlListPicker/SqlListPickerPreValueEditor.cs
@@ -0,0 +1,25 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.SqlListPicker
+{
+    using nuComponents.DataTypes.Interfaces;
+    using Umbraco.Core.PropertyEditors;
+
+    internal class SqlListPickerPreValueEditor : PreValueEditor, IPickerPreValueEditor
+    {
+        // get data
+        [PreValueField("dataSource", "", "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfig.html", HideLabel = true)]
+        public string DataSource { get; set; }
+
+        // transform data
+        [PreValueField("customLabel", "Label Macro", "App_Plugins/nuComponents/DataTypes/Shared/CustomLabel/CustomLabelConfig.html", HideLabel = true)]
+        public string CustomLabel { get; set; }
+
+        // present data
+        [PreValueField("listPicker", "", "App_Plugins/nuComponents/DataTypes/Shared/ListPicker/ListPickerConfig.html", HideLabel = true)]
+        public string ListPicker { get; set; }
+
+        // wire up
+        [PreValueField("apiController", "SqlListPickerApi", "App_Plugins/nuComponents/DataTypes/Shared/HiddenConstant/HiddenConstantConfig.html", HideLabel = true)]
+        public string ApiController { get; set; }
+    }
+}
---
+++ source/nuComponents.DataTypes/PropertyEditors/SqlListPicker/SqlListPickerPropertyEditor.cs
@@ -0,0 +1,27 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.SqlListPicker
+{
+    using ClientDependency.Core;
+    using Umbraco.Core.PropertyEditors;
+    using Umbraco.Web.PropertyEditors;
+
+    // EDITOR UI
+    [PropertyEditor("sqlListPicker", "nuComponents: Sql List Picker", "App_Plugins/nuComponents/DataTypes/Shared/ListPicker/ListPickerEditor.html", ValueType = "TEXT")]
+    [PropertyEditorAsset(ClientDependencyType.Css, "App_Plugins/nuComponents/DataTypes/Shared/ListPicker/ListPickerEditor.css")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/ListPicker/ListPickerEditorController.js")]
+
+    // RESOURCES
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/Picker/PickerResource.js")]
+
+    // CONFIG
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfigController.js")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/CustomLabel/CustomLabelConfigController.js")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/ListPicker/ListPickerConfigController.js")]
+    public class SqlListPickerPropertyEditor : PropertyEditor
+    {
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new SqlListPickerPreValueEditor();
+        }
+    }
+}
