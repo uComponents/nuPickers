7dae78a16fbcab3d08333e3f0632108ccd2a0850
Hendy Racher <git@hendyracher.co.uk>
2014-05-11 18:59:38 +0100

    added SqlCheckBoxPicker

---
+++ source/nuComponents.DataTypes/PropertyEditors/SqlCheckBoxPicker/SqlCheckBoxPickerPreValueEditor.cs
@@ -0,0 +1,21 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.SqlCheckBoxPicker
+{
+    using Umbraco.Core.PropertyEditors;
+    using nuComponents.DataTypes.Interfaces;
+
+    internal class SqlCheckBoxPickerPreValueEditor : PreValueEditor, IPickerPreValueEditor
+    {
+        [PreValueField("dataSource", "", "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfig.html", HideLabel = true)]
+        public string DataSource { get; set; }
+
+        [PreValueField("customLabel", "Label Macro", "App_Plugins/nuComponents/DataTypes/Shared/CustomLabel/CustomLabelConfig.html", HideLabel = true)]
+        public string CustomLabel { get; set; }
+
+        [PreValueField("checkBoxPicker", "", "App_Plugins/nuComponents/DataTypes/Shared/CheckBoxPicker/CheckBoxPickerConfig.html", HideLabel = true)]
+        public string CheckBoxPicker { get; set; }
+
+        [PreValueField("apiController", "SqlCheckBoxPickerApi", "App_Plugins/nuComponents/DataTypes/Shared/HiddenConstant/HiddenConstantConfig.html", HideLabel = true)]
+        public string ApiController { get; set; }
+    }
+}
---
+++ source/nuComponents.DataTypes/PropertyEditors/SqlCheckBoxPicker/SqlCheckBoxPickerPropertyEditor.cs
@@ -0,0 +1,26 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.SqlCheckBoxPicker
+{
+    using ClientDependency.Core;
+    using Umbraco.Core.PropertyEditors;
+    using Umbraco.Web.PropertyEditors;
+
+    // EDITOR UI
+    [PropertyEditor("sqlCheckBoxPicker", "nuComponents: Sql CheckBox Picker", "App_Plugins/nuComponents/DataTypes/Shared/CheckBoxPicker/CheckBoxPickerEditor.html", ValueType = "TEXT")]
+    [PropertyEditorAsset(ClientDependencyType.Css, "App_Plugins/nuComponents/DataTypes/Shared/CheckBoxPicker/CheckBoxPickerEditor.css")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/CheckBoxPicker/CheckBoxPickerEditorController.js")]
+
+    // RESOURCES
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/Picker/PickerResource.js")]
+
+    // CONFIG
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfigController.js")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/CustomLabel/CustomLabelConfigController.js")]
+    public class SqlCheckBoxPickerPropertyEditor : PropertyEditor
+    {
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new SqlCheckBoxPickerPreValueEditor();
+        }
+    }
+}
