538513648a5350c4878d6537a971e89d6883b00f
Hendy Racher <git@hendyracher.co.uk>
2014-05-11 16:41:12 +0100

    added EnumCheckBoxPicker

---
+++ source/nuComponents.DataTypes/PropertyEditors/EnumCheckBoxPicker/EnumCheckBoxPickerPreValueEditor.cs
@@ -0,0 +1,18 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.EnumCheckBoxPicker
+{
+    using Umbraco.Core.PropertyEditors;
+    using nuComponents.DataTypes.Interfaces;
+
+    internal class EnumCheckBoxPickerPreValueEditor : PreValueEditor, IPickerPreValueEditor
+    {
+        [PreValueField("dataSource", "", "App_Plugins/nuComponents/DataTypes/Shared/EnumDataSource/EnumDataSourceConfig.html", HideLabel = true)]
+        public string DataSource { get; set; }
+
+        /// <summary>
+        /// NOTE: the name 'EnumDropDownPickerApi' is injected into the 'apiController' hidden field value
+        /// </summary>
+        [PreValueField("apiController", "EnumCheckBoxPickerApi", "App_Plugins/nuComponents/DataTypes/Shared/HiddenConstant/HiddenConstantConfig.html", HideLabel = true)]
+        public string ApiController { get; set; }
+    }
+}
---
+++ source/nuComponents.DataTypes/PropertyEditors/EnumCheckBoxPicker/EnumCheckBoxPickerPropertyEditor.cs
@@ -0,0 +1,24 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.EnumCheckBoxPicker
+{
+    using ClientDependency.Core;
+    using Umbraco.Core.PropertyEditors;
+    using Umbraco.Web.PropertyEditors;
+
+    // EDITOR UI
+    [PropertyEditor("EnumCheckBoxPicker", "nuComponents: Enum CheckBox Picker", "App_Plugins/nuComponents/DataTypes/Shared/CheckBoxPicker/CheckBoxPickerEditor.html", ValueType = "TEXT")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/CheckBoxPicker/CheckBoxPickerEditorController.js")]
+
+    // RESOURCES
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/Picker/PickerResource.js")]
+
+    // CONFIG
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/EnumDataSource/EnumDataSourceConfigController.js")]
+    public class EnumCheckBoxPickerPropertyEditor : PropertyEditor
+    {
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new EnumCheckBoxPickerPreValueEditor();
+        }
+    }
+}
