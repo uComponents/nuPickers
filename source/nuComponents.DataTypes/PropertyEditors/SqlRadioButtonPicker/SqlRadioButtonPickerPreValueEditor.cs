ff0ce59c171c7659f69a7aa3f55ff7bdb447ee17
Hendy Racher <git@hendyracher.co.uk>
2014-05-11 19:14:50 +0100

    added SqlRadioButtonPicker

---
+++ source/nuComponents.DataTypes/PropertyEditors/SqlRadioButtonPicker/SqlRadioButtonPickerPreValueEditor.cs
@@ -0,0 +1,21 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.SqlRadioButtonPicker
+{
+    using Umbraco.Core.PropertyEditors;
+    using nuComponents.DataTypes.Interfaces;
+
+    internal class SqlRadioButtonPickerPreValueEditor : PreValueEditor, IPickerPreValueEditor
+    {
+        [PreValueField("dataSource", "", "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfig.html", HideLabel = true)]
+        public string DataSource { get; set; }
+
+        [PreValueField("customLabel", "Label Macro", "App_Plugins/nuComponents/DataTypes/Shared/CustomLabel/CustomLabelConfig.html", HideLabel = true)]
+        public string CustomLabel { get; set; }
+
+        [PreValueField("radioButtonPicker", "", "App_Plugins/nuComponents/DataTypes/Shared/RadioButtonPicker/RadioButtonPickerConfig.html", HideLabel = true)]
+        public string RadioButtonPicker { get; set; }
+
+        [PreValueField("apiController", "SqlRadioButtonPickerApi", "App_Plugins/nuComponents/DataTypes/Shared/HiddenConstant/HiddenConstantConfig.html", HideLabel = true)]
+        public string ApiController { get; set; }
+    }
+}
---
+++ source/nuComponents.DataTypes/PropertyEditors/SqlRadioButtonPicker/SqlRadioButtonPickerPropertyEditor.cs
@@ -0,0 +1,25 @@
+﻿namespace nuComponents.DataTypes.PropertyEditors.SqlRadioButtonPicker
+{
+    using ClientDependency.Core;
+    using Umbraco.Core.PropertyEditors;
+    using Umbraco.Web.PropertyEditors;
+
+    // EDITOR UI
+    [PropertyEditor("sqlRadioButtonPicker", "nuComponents: Sql RadioButton Picker", "App_Plugins/nuComponents/DataTypes/Shared/RadioButtonPicker/RadioButtonPickerEditor.html", ValueType = "TEXT")]
+    [PropertyEditorAsset(ClientDependencyType.Css, "App_Plugins/nuComponents/DataTypes/Shared/RadioButtonPicker/RadioButtonPickerEditor.css")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/RadioButtonPicker/RadioButtonPickerEditorController.js")]
+
+    // RESOURCES
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/Picker/PickerResource.js")]
+
+    // CONFIG
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfigController.js")]
+    [PropertyEditorAsset(ClientDependencyType.Javascript, "App_Plugins/nuComponents/DataTypes/Shared/CustomLabel/CustomLabelConfigController.js")]
+    public class SqlRadioButtonPickerPropertyEditor : PropertyEditor
+    {
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new SqlRadioButtonPickerPreValueEditor();
+        }
+    }
+}
