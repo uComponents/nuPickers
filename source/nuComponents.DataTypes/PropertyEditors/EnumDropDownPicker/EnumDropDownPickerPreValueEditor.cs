920ad3ca58304971272db8b85311d12d2b4f017d
Hendy Racher <git@hendyracher.co.uk>
2014-05-17 01:06:17 +0100

    added SaveFormat config to SqlDropDownPicker and EnumDropDownPicker

--- source/nuComponents.DataTypes/PropertyEditors/EnumDropDownPicker/EnumDropDownPickerPreValueEditor.cs
+++ source/nuComponents.DataTypes/PropertyEditors/EnumDropDownPicker/EnumDropDownPickerPreValueEditor.cs
@@ -8,9 +8,9 @@
         [PreValueField("dataSource", "", "App_Plugins/nuComponents/DataTypes/Shared/EnumDataSource/EnumDataSourceConfig.html", HideLabel = true)]
         public string DataSource { get; set; }
 
-        /// <summary>
-        /// NOTE: the name 'EnumDropDownPickerApi' is injected into the 'apiController' hidden field value
-        /// </summary>
+        [PreValueField("saveFormat", "Save Format", "App_Plugins/nuComponents/DataTypes/Shared/SaveFormat/SaveFormatConfig.html")]
+        public string SaveFormat { get; set; }
+
         [PreValueField("apiController", "EnumDropDownPickerApi", "App_Plugins/nuComponents/DataTypes/Shared/HiddenConstant/HiddenConstantConfig.html", HideLabel = true)]
         public string ApiController { get; set; }
     }
--- source/nuComponents.DataTypes/PropertyEditors/SqlDropDownPicker/SqlDropDownPickerPreValueEditor.cs
+++ source/nuComponents.DataTypes/PropertyEditors/SqlDropDownPicker/SqlDropDownPickerPreValueEditor.cs
@@ -8,9 +8,9 @@
         [PreValueField("dataSource", "", "App_Plugins/nuComponents/DataTypes/Shared/SqlDataSource/SqlDataSourceConfig.html", HideLabel = true)]
         public string DataSource { get; set; }
 
-        /// <summary>
-        /// NOTE: the name 'SqlDropDownPickerApi' is injected into the 'apiController' hidden field value
-        /// </summary>
+        [PreValueField("saveFormat", "Save Format", "App_Plugins/nuComponents/DataTypes/Shared/SaveFormat/SaveFormatConfig.html")]
+        public string SaveFormat { get; set; }
+
         [PreValueField("apiController", "SqlDropDownPickerApi", "App_Plugins/nuComponents/DataTypes/Shared/HiddenConstant/HiddenConstantConfig.html", HideLabel = true)]
         public string ApiController { get; set; }
     }
