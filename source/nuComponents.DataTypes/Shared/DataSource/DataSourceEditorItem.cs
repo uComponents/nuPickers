3861a363b2f3e58a80c58ec18be231ea99bd6996
Hendy Racher <git@hendyracher.co.uk>
2014-05-23 10:46:26 +0100

    note to change PickerEditorOption references to DataSourceEditorItem

--- source/nuComponents.DataTypes/Shared/DataSource/DataSourceEditorItem.cs
+++ source/nuComponents.DataTypes/Shared/DataSource/DataSourceEditorItem.cs
@@ -3,14 +3,11 @@ namespace nuComponents.DataTypes.Shared.Picker
 {
     using Newtonsoft.Json;
 
-    /// <summary>
-    /// would like to rename this to DataSourceEditorItem (consequence = lots of changes)
-    /// </summary>
-    public class PickerEditorOption
+    public class DataSourceEditorItem
     {
         [JsonProperty("key")]
         public string Key { get; set; }
-     
+
         [JsonProperty("label")]
         public string Label { get; set; }
     }
