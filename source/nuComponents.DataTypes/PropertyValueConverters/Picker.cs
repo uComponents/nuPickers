d79caebc4d5634fcb3424070fcfc20b0bd66eea9
Hendy Racher <git@hendyracher.co.uk>
2014-06-04 13:51:06 +0100

    added PropertyValueConverter for all Pickers

---
+++ source/nuComponents.DataTypes/PropertyValueConverters/Picker.cs
@@ -0,0 +1,69 @@
+﻿namespace nuComponents.DataTypes.PropertyValueConverters
+{
+    using nuComponents.DataTypes.Shared.SaveFormat;
+    using System.Collections.Generic;
+    using Umbraco.Core;
+    using Umbraco.Core.Models;
+
+    public class Picker
+    {
+        protected int ContextId { get; private set; }
+        protected int DataTypeId { get; private set; }
+        protected object SavedValue { get; private set; }
+
+        private IEnumerable<PreValue> dataTypePreValues = null;
+        protected IEnumerable<PreValue> DataTypePreValues
+        {
+            get
+            {
+                if (this.dataTypePreValues == null)
+                {
+                    this.dataTypePreValues = ApplicationContext
+                                                .Current
+                                                .Services
+                                                .DataTypeService
+                                                .GetPreValuesCollectionByDataTypeId(this.DataTypeId)
+                                                .PreValuesAsArray;
+                }
+
+                return this.dataTypePreValues;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="contextId">the id of the (content, media or member) item being edited</param>
+        /// <param name="dataTypeId">the id of the datatype (a property editor instance) this allows access to all prevalues</param>
+        /// <param name="savedValue">the actual value saved</param>
+        internal Picker(int contextId, int dataTypeId, object savedValue)
+        {
+            this.ContextId = contextId;
+            this.DataTypeId = dataTypeId;
+            this.SavedValue = savedValue;
+        }
+
+        public IEnumerable<string> PickedKeys
+        {
+            get
+            {
+                // ignore the specified saved format, and let save format try and restore collection directly from the saved value
+                return SaveFormat.GetSavedKeys(this.SavedValue.ToString());
+            }
+        }
+        
+        /// <summary>
+        /// As this object can be cached (until context item is saved) relations need to be queried every time.
+        /// </summary>
+        public IEnumerable<int> RelatedIds
+        {
+            get
+            {                
+
+               // does this data-type map to relations at all ?
+
+                return null;
+            }
+        }
+    }
+}
---
+++ source/nuComponents.DataTypes/PropertyValueConverters/PickerPropertyValueConverter.cs
@@ -0,0 +1,45 @@
+﻿
+namespace nuComponents.DataTypes.PropertyValueConverters
+{
+    using System.Linq;
+    using Umbraco.Core.PropertyEditors;
+    using Umbraco.Core.Models.PublishedContent;
+    using nuComponents.DataTypes.PropertyEditors;
+
+    [PropertyValueType(typeof(Picker))]
+    [PropertyValueCache(PropertyCacheValue.All, PropertyCacheLevel.Content)]
+    public class PickerPropertyValueConverter : PropertyValueConverterBase
+    {
+        /// <summary>
+        /// This is a generic converter for ALL nuComponents Picker PropertyEditors
+        /// this may be subclassed in the future for specific types (eg. Enum collection properties, or Xml and Content / Media / Members etc)
+        /// </summary>
+        /// <param name="propertyType"></param>
+        /// <returns></returns>
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return new string[] { 
+                PropertyEditorConstants.EnumCheckBoxPickerAlias,
+                PropertyEditorConstants.EnumDropDownPickerAlias,
+                PropertyEditorConstants.EnumPrefetchListPickerAlias,
+                PropertyEditorConstants.EnumRadioButtonPickerAlias,
+                PropertyEditorConstants.SqlCheckBoxPickerAlias,
+                PropertyEditorConstants.SqlDropDownPickerAlias,
+                PropertyEditorConstants.SqlPrefetchListPickerAlias,
+                PropertyEditorConstants.SqlRadioButtonPickerAlias,
+                PropertyEditorConstants.SqlTypeaheadListPickerAlias,
+                PropertyEditorConstants.XmlCheckBoxPickerAlias,
+                PropertyEditorConstants.XmlDropDownPickerAlias,
+                PropertyEditorConstants.XmlPrefetchListPickerAlias,
+                PropertyEditorConstants.XmlRadioButtonPickerAlias,
+                PropertyEditorConstants.XmlTypeaheadListPickerAlias
+            }
+            .Contains(propertyType.PropertyTypeAlias);
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return new Picker(propertyType.ContentType.Id, propertyType.DataTypeId, source);
+        }
+    }
+}
