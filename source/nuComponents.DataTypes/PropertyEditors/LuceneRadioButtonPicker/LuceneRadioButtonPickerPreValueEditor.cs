40c5bb1706c2e47ceaaa33dc94242099a782ac93
Hendy Racher <git@hendyracher.co.uk>
2014-06-25 06:37:36 +0100

    added Lucene Pickers

---
+++ source/nuComponents.DataTypes/PropertyEditors/LuceneRadioButtonPicker/LuceneRadioButtonPickerPreValueEditor.cs
@@ -0,0 +1,23 @@
+﻿
+namespace nuComponents.DataTypes.PropertyEditors.LuceneRadioButtonPicker
+{
+    using Umbraco.Core.PropertyEditors;
+
+    internal class LuceneRadioButtonPickerPreValueEditor : PreValueEditor
+    {
+        [PreValueField("dataSource", "", EmbeddedResource.RootUrl + "LuceneDataSource/LuceneDataSourceConfig.html", HideLabel = true)]
+        public string DataSource { get; set; }
+
+        [PreValueField("customLabel", "Label Macro", EmbeddedResource.RootUrl + "CustomLabel/CustomLabelConfig.html", HideLabel = true)]
+        public string CustomLabel { get; set; }
+
+        [PreValueField("layoutDirection", "Layout Direction", EmbeddedResource.RootUrl + "LayoutDirection/LayoutDirectionConfig.html")]
+        public string LayoutDirection { get; set; }
+
+        [PreValueField("relationMapping", "", EmbeddedResource.RootUrl + "RelationMapping/RelationMappingConfig.html", HideLabel = true)]
+        public string RelationMapping { get; set; }
+
+        [PreValueField("saveFormat", "Save Format", EmbeddedResource.RootUrl + "SaveFormat/SaveFormatConfig.html")]
+        public string SaveFormat { get; set; }
+    }
+}
