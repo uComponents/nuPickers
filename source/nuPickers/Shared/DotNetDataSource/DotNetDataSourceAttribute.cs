611d429bbbd4db0264522dcc6027d31d613326ab
Hendy Racher <git@hendyracher.co.uk>
2014-07-02 23:37:14 +0100

    added DotNetDataSourceAttribute

---
+++ source/nuPickers/Shared/DotNetDataSource/DotNetDataSourceAttribute.cs
@@ -0,0 +1,16 @@
+﻿
+namespace nuPickers.Shared.DotNetDataSource
+{
+    using System;
+
+    /// <summary>
+    /// When this attribute is used, the pickers will render a corresponding config field
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class DotNetDataSourceAttribute : Attribute
+    {
+        public DotNetDataSourceAttribute()
+        {
+        }
+    }
+}
