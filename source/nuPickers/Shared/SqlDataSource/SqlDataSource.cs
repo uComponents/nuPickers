5ad7cd979fb254e4fa8380e83101e237d934b3e0
Hendy Racher <git@hendyracher.co.uk>
2014-07-06 12:36:28 +0100

    fix for #26 & #27 - replaced ISqlHelper with Umbraco.Core.Persistence.Database

--- source/nuPickers/Shared/SqlDataSource/SqlDataSource.cs
+++ source/nuPickers/Shared/SqlDataSource/SqlDataSource.cs
@@ -1,15 +1,10 @@
 ﻿
 namespace nuPickers.Shared.SqlDataSource
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Configuration;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using umbraco.DataLayer;
     using nuPickers.Shared.Editor;
+    using System.Collections.Generic;
     using System.Text.RegularExpressions;
+    using Umbraco.Core.Persistence;
 
     public class SqlDataSource
     {
@@ -23,42 +18,16 @@ namespace nuPickers.Shared.SqlDataSource
         {
             List<EditorDataItem> editorDataItems = new List<EditorDataItem>();
 
-            ConnectionStringSettings connectionStringSettings = ConfigurationManager.ConnectionStrings[this.ConnectionString];            
-            if (connectionStringSettings != null)
+            Database database = new Database(this.ConnectionString);
+
+            if (database != null)
             {
-                using (ISqlHelper sqlHelper = DataLayerHelper.CreateSqlHelper(connectionStringSettings.ConnectionString))
-                {
-                    string sql = Regex.Replace(this.SqlExpression, "\n|\r", " ");
-                    List<IParameter> parameters = new List<IParameter>();
+                string sql = Regex.Replace(this.SqlExpression, "\n|\r", " ")
+                             .Replace("@contextId", "@0")
+                             .Replace("@typeahead", "@1");
 
-                    if (sql.Contains("@contextId"))
-                    {
-                        parameters.Add(sqlHelper.CreateParameter("@contextId", contextId));
-                    }
 
-                    if (sql.Contains("@typeahead"))
-                    {
-                        // if @typeahead is specified in the sql, then supply it - this can be useful to reduce the result set used by the type ahead filtering later
-                        parameters.Add(sqlHelper.CreateParameter("@typeahead", this.Typeahead));
-                    }
-
-                    using(IRecordsReader recordsReader = sqlHelper.ExecuteReader(sql, parameters.ToArray()))
-                    {
-                        if(recordsReader != null)
-                        {
-                            while(recordsReader.Read())
-                            {
-                                editorDataItems.Add(
-                                    new EditorDataItem()
-                                    {
-                                        Key = recordsReader.GetObject("Key").ToString(),
-                                        Label = recordsReader.GetObject("Label").ToString()
-                                    }
-                                );
-                            }
-                        }
-                    }
-                }
+                editorDataItems = database.Fetch<EditorDataItem>(sql, contextId, this.Typeahead);
             }
 
             return editorDataItems;
