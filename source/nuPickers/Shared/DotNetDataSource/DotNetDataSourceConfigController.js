edbb1f7059648cdf219519d220a15c716e047960
Hendy Racher <git@hendyracher.co.uk>
2014-07-19 21:15:15 +0100

    DotNet DataSource config - fixed js issue when no properties returned

--- source/nuPickers/Shared/DotNetDataSource/DotNetDataSourceConfigController.js
+++ source/nuPickers/Shared/DotNetDataSource/DotNetDataSourceConfigController.js
@@ -14,16 +14,22 @@ angular
 
             if ($scope.model.value.className != null) {
                 $http.get('backoffice/nuPickers/DotNetDataSourceApi/GetProperties',
-                    { params: { assemblyName: $scope.model.value.assemblyName, className: $scope.model.value.className } })
+                    {
+                        params: {
+                            assemblyName: $scope.model.value.assemblyName,
+                            className: $scope.model.value.className
+                        }
+                    })
                     .then(function (response) {
-                        $scope.properties = response.data;
+                        if (response.data != 'null') {
+                            $scope.properties = response.data;
 
-                        if ($scope.model.value.properties) {
-                            // TODO: simplify
-                            for (var i = 0; i < $scope.properties.length; i++) {
-                                for (var j = 0; j < $scope.model.value.properties.length; j++) {
-                                    if ($scope.properties[i].name == $scope.model.value.properties[j].name) {
-                                        $scope.properties[j].value = $scope.model.value.properties[j].value;
+                            if ($scope.model.value.properties) {
+                                for (var i = 0; i < $scope.properties.length; i++) {
+                                    for (var j = 0; j < $scope.model.value.properties.length; j++) {
+                                        if ($scope.properties[i].name == $scope.model.value.properties[j].name) {
+                                            $scope.properties[i].value = $scope.model.value.properties[j].value;
+                                        }
                                     }
                                 }
                             }
@@ -32,43 +38,40 @@ angular
             }
         };
 
-        // get collection of assemblies
-        $http.get('backoffice/nuPickers/DotNetDataSourceApi/GetAssemblyNames').then(function (response) {
+        $scope.$watch('model.value.assemblyName', function () {
 
-            $scope.assemblyNames = response.data;
+            $scope.classNames = null; // clear, incase new ones can't be set
+            $scope.properties = null;
 
-            // this always runs on startup
-            $scope.$watch('model.value.assemblyName', function () {
-
-                $scope.classNames = null; // clear, incase new ones can't be set
-                $scope.properties = null;
-
-                if ($scope.model.value.assemblyName != null) {
-                    $http.get('backoffice/nuPickers/DotNetDataSourceApi/GetClassNames',
-                        { params: { assemblyName: $scope.model.value.assemblyName } })
-                        .then(function (response) {
-                            $scope.classNames = response.data;
-
-                            $scope.buildProperties(); // (re)build properties collection
-                        });
-                }
-            });
-            
-            $scope.$watch('model.value.className', function () {
-                $scope.buildProperties();
-            });            
-
-
-            $scope.$on("formSubmitting", function () {
-
-                $scope.model.value.properties = null;
-                $scope.model.value.properties = $scope.properties.map(function (property) {
-                    return { 'name': property.name, 'value': property.value }
-                });
-
-            });
+            if ($scope.model.value.assemblyName != null) {
+                $http.get('backoffice/nuPickers/DotNetDataSourceApi/GetClassNames',
+                    {
+                        params: { assemblyName: $scope.model.value.assemblyName }
+                    })
+                    .then(function (response) {
+                        $scope.classNames = response.data;
+                        $scope.buildProperties(); // (re)build properties collection
+                    });
+            }
         });
 
+        $scope.$watch('model.value.className', function () {
+            $scope.buildProperties();
+        });
+
+        $scope.$on("formSubmitting", function () {
+
+            $scope.model.value.properties = null;
+            $scope.model.value.properties = $scope.properties.map(function (property) {
+                return { 'name': property.name, 'value': property.value }
+            });
+
+        });
+
+        // set collection of assemblies
+        $http.get('backoffice/nuPickers/DotNetDataSourceApi/GetAssemblyNames').then(function (response) {
+            $scope.assemblyNames = response.data;
+        });
 
     }]);
 
