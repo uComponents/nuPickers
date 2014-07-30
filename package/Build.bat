58ab177a49e6b9cc259320f07497d8ecc35418a2
Jeavon Leopold <jeavon@crumpled-dog.com>
2014-07-30 10:47:13 +0100

    Setting up package build scripts for AppVeyor

--- package/Build.bat
+++ package/Build.bat
@@ -1,1 +1,2 @@
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe .\package.proj
+Call ..\source\.nuget\nuget.exe restore ..\source\nuPickers.sln
+"C:\Program Files (x86)\MSBuild\12.0\Bin\MsBuild.exe" .\package.proj
