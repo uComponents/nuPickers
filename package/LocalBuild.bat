c5bb6097e76f8ca58940c1f0039b3c03c1fed79f
Jeavon Leopold <jeavon@crumpled-dog.com>
2015-03-12 19:21:15 +0000

    Update Build scripts to latest version

--- package/LocalBuild.bat
+++ package/LocalBuild.bat
@@ -1,7 +1,24 @@
-set APPVEYOR_BUILD_NUMBER=100
-set APPVEYOR_BUILD_VERSION=1.0.0.100
-set UMBRACO_PACKAGE_MIN_VERSION=7.1.3
-set UMBRACO_PACKAGE_PRERELEASE_SUFFIX=
-REM ** Generally branch should be either master or Release **
-set APPVEYOR_REPO_BRANCH=master
+ECHO off
+
+SET /P APPVEYOR_BUILD_NUMBER=Please enter a build number (e.g. 134):
+SET /P PACKAGE_VERISON=Please enter your package version (e.g. 1.0.5):
+SET /P UMBRACO_PACKAGE_PRERELEASE_SUFFIX=Please enter your package release suffix or leave empty (e.g. beta):
+
+SET /P APPVEYOR_REPO_TAG=If you want to simulate a GitHub tag for a release (e.g. true):
+
+if "%APPVEYOR_BUILD_NUMBER%" == "" (
+  SET APPVEYOR_BUILD_NUMBER=100
+)
+if "%PACKAGE_VERISON%" == "" (
+  SET PACKAGE_VERISON=0.1.0
+)
+
+SET APPVEYOR_BUILD_VERSION=%PACKAGE_VERISON%.%APPVEYOR_BUILD_NUMBER%
+
 build.bat
+
+@IF %ERRORLEVEL% NEQ 0 GOTO err
+@EXIT /B 0
+:err
+@PAUSE
+@EXIT /B 1
