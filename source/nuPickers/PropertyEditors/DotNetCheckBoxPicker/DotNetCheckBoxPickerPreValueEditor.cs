b09b2069704d822e456ef194cf8e2af8953b1243
Hendy Racher <git@hendyracher.co.uk>
2015-05-02 00:50:32 +0100

    added a config option to all check box pickers

--- source/nuPickers/PropertyEditors/DotNetCheckBoxPicker/DotNetCheckBoxPickerPreValueEditor.cs
+++ source/nuPickers/PropertyEditors/DotNetCheckBoxPicker/DotNetCheckBoxPickerPreValueEditor.cs
@@ -10,6 +10,9 @@
         [PreValueField("customLabel", "", EmbeddedResource.RootUrl + "CustomLabel/CustomLabelConfig.html", HideLabel = true)]
         public string CustomLabel { get; set; }
 
+        [PreValueField("checkBoxPicker", "", EmbeddedResource.RootUrl + "CheckBoxPicker/CheckBoxPickerConfig.html", HideLabel = true)]
+        public string CheckBoxPicker { get; set; }
+
         [PreValueField("layoutDirection", "Layout Direction", EmbeddedResource.RootUrl + "LayoutDirection/LayoutDirectionConfig.html")]
         public string LayoutDirection { get; set; }
 
