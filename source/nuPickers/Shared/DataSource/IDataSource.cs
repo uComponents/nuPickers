bba779381b05113932aa848344dbe49691fc2882
Hendy Racher <git@hendyracher.co.uk>
2017-04-02 22:28:43 +0100

    added new interface IDataSource (to be implemented on all data sources), and a new static class Editor with method to get all items for a picker

---
+++ source/nuPickers/Shared/DataSource/IDataSource.cs
@@ -0,0 +1,31 @@
+﻿
+namespace nuPickers.Shared.DataSource
+{
+    using nuPickers.Shared.Editor;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// All datasources must implement this, so that Editor.GetGetEditorDataItems() can be used by all API controllers & the Picker obj
+    /// </summary>
+    internal interface IDataSource
+    {
+        /// <summary>
+        /// set any typeahead text so that it may be handled by the data source
+        /// </summary>
+        string Typeahead {set; }
+
+        /// <summary>
+        /// flag to indicate whether the datasource handled any type ahead text
+        /// (if the datasource doesn't handle it directly, then the typeahead text is processed later)
+        /// </summary>
+        bool HandledTypeahead { get; }
+
+        /// <summary>
+        /// the main method on the datasource to return the data items
+        /// </summary>
+        /// <param name="currentId">the current id</param>
+        /// <param name="parentId">the parent id</param>
+        /// <returns>collection of <see cref="EditorDataItem"/> POCOs that are used as options for a picker</returns>
+        IEnumerable<EditorDataItem> GetEditorDataItems(int currentId, int parentId);
+    }
+}
---
+++ source/nuPickers/Shared/Editor/Editor.cs
@@ -0,0 +1,49 @@
+﻿
+namespace nuPickers.Shared.Editor
+{
+    using DataSource;
+    using nuPickers.Shared.CustomLabel;
+    using nuPickers.Shared.TypeaheadListPicker;
+    using System.Collections.Generic;
+
+    internal static class Editor
+    {
+        /// <summary>
+        /// Get all options for a picker
+        /// </summary>
+        /// <param name="currentId">the current id</param>
+        /// <param name="parentId">the parent id</param>
+        /// <param name="propertyAlias">the property alias</param>
+        /// <param name="dataSource">the datasource</param>
+        /// <param name="customLabelMacro">an optional macro to use for custom labels</param>
+        /// <param name="typeahead">optional typeahead text to filter down on items returned</param>
+        /// <returns></returns>
+        internal static IEnumerable<EditorDataItem> GetEditorDataItems(                                                        
+                                                        int currentId,
+                                                        int parentId,
+                                                        string propertyAlias, // used in custom label
+                                                        IDataSource dataSource, 
+                                                        string customLabelMacro,
+                                                        string typeahead)
+        {
+            dataSource.Typeahead = typeahead; // set any typeahead text that the datasource may filter on
+
+            IEnumerable<EditorDataItem> editorDataItems = dataSource.GetEditorDataItems(currentId, parentId);
+ 
+            if (!string.IsNullOrWhiteSpace(customLabelMacro))
+            {
+                editorDataItems = new CustomLabel(customLabelMacro, currentId, propertyAlias)
+                                        .ProcessEditorDataItems(editorDataItems);
+            }
+
+            // if the datasource didn't handle the typeahead text, then it needs to be done post custom label processing
+            if (!dataSource.HandledTypeahead)
+            {
+                editorDataItems = new TypeaheadListPicker(typeahead)
+                                        .ProcessEditorDataItems(editorDataItems);
+            }
+
+            return editorDataItems;
+        }
+    }
+}
