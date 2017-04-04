c202169efbb8b69360f1413619bf2ab30e82f03a
Hendy Racher <git@hendyracher.co.uk>
2017-04-04 19:43:23 +0100

    stubbed out new static method: SaveFormat.TryGetSavedItems()

--- source/nuPickers/Shared/SaveFormat/SaveFormat.cs
+++ source/nuPickers/Shared/SaveFormat/SaveFormat.cs
@@ -1,6 +1,6 @@
-﻿
-namespace nuPickers.Shared.SaveFormat
+﻿namespace nuPickers.Shared.SaveFormat
 {
+    using Editor;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using System.Collections.Generic;
@@ -10,9 +10,9 @@ namespace nuPickers.Shared.SaveFormat
     internal static class SaveFormat
     {
         /// <summary>
-        /// ignore the specified saved format, and try and restore collection directly from the saved value
+        /// Ignore the specified saved format, and try and restore collection directly from the saved value
         /// </summary>
-        /// <param name="savedValue"></param>
+        /// <param name="savedValue">the saved value as a string</param>
         /// <returns></returns>
         internal static IEnumerable<string> GetSavedKeys(string savedValue)
         {
@@ -29,11 +29,24 @@ namespace nuPickers.Shared.SaveFormat
                         return XDocument.Parse(savedValue).Descendants("Picked").Select(x => x.Attribute("Key").Value);
 
                     default: // csv
-                        return savedValue.Split(',');                          
+                        return savedValue.Split(',');
                 }
             }
 
             return Enumerable.Empty<string>();
+        }
+
+
+        /// <summary>
+        /// Attempt to create a collection of the picked (key/label) items from the saved string value
+        /// NOTE: the CSV format, or relations only will not work, as the data isn't in the saved value (will return false)
+        /// </summary>
+        /// <param name="savedValue">the saved value as a string</param>
+        /// <param name="items"></param>
+        /// <returns>bool flag to indicate whether the items could be created from the savedValue supplied</returns>
+        internal static bool TryGetSavedItems(string savedValue, out IEnumerable<EditorDataItem> items)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
