afa4471ea740d7b71d0d3e0e0e0080b443b6910a
Hendy Racher <git@hendyracher.co.uk>
2017-04-07 23:44:25 +0100

    .GetPicker(propertyAlias) extension method on IPublishedContent #58 #163

---
+++ source/nuPickers/Extensions/IPublishedContentExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace nuPickers.Extensions
+{
+    using Umbraco.Core.Models;
+
+    public static class IPublishedContentExtensions
+    {
+        /// <summary>
+        /// Get a Picker model for the supplied propertyAlias on this <see cref="IPublishedContent"/>  
+        /// </summary>
+        /// <param name="publishedContent"></param>
+        /// <param name="propertyAlias"></param>
+        /// <returns></returns>
+        public static Picker GetPicker(this IPublishedContent publishedContent, string propertyAlias)
+        {
+            return new Picker(publishedContent.Id, propertyAlias);
+        }
+    }
+}
