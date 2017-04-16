dda6e4035e861f607be3aec5379bdac5aa2d702f
Hendy Racher <git@hendyracher.co.uk>
2017-04-16 19:21:23 +0100

    save format config no longer attempts to restrict csv for typeaheads #135

--- source/nuPickers/Shared/SaveFormat/SaveFormatConfigController.js
+++ source/nuPickers/Shared/SaveFormat/SaveFormatConfigController.js
@@ -3,29 +3,19 @@ angular
     .module("umbraco")
     .controller("nuPickers.Shared.SaveFormat.SaveFormatConfigController",
     ['$rootScope', '$scope', function ($rootScope, $scope) {
-        
-        $scope.isTypeaheadListPicker = false;
-        $scope.$on('isTypeaheadListPicker', function (event, arg) { $scope.isTypeaheadListPicker = arg; });
-        $rootScope.$broadcast('saveFormatListening');
 
         $scope.relationMappingBidirectional = false;
         
-        if (!$scope.isTypeaheadListPicker) {
+        $scope.model.value = $scope.model.value || 'csv';
 
-            $scope.model.value = $scope.model.value || 'csv';
+        $scope.$on('relationMappingChanged', function (event, relationType) {
+            $scope.relationMappingBidirectional = (relationType != null && relationType.bidirectional);
 
-            $scope.$on('relationMappingChanged', function (event, relationType) {
-                $scope.relationMappingBidirectional = (relationType != null && relationType.bidirectional);
+            if ($scope.model.value == 'relationsOnly' && !$scope.relationMappingBidirectional) {
+                $scope.model.value = 'csv'; // fallback to csv
+            }
+        });
 
-                if ($scope.model.value == 'relationsOnly' && !$scope.relationMappingBidirectional) {
-                    $scope.model.value = 'csv'; // fallback to csv
-                }
-            });
-
-        }
-        else { // it's a typeahead so only allow formats that save both key and label
-            $scope.model.value = $scope.model.value || 'json';
-        }
 
 
     }]);
--- source/nuPickers/Shared/TypeaheadListPicker/TypeaheadListPickerConfigController.js
+++ source/nuPickers/Shared/TypeaheadListPicker/TypeaheadListPickerConfigController.js
@@ -4,10 +4,4 @@ angular
     .controller("nuPickers.Shared.TypeaheadListPicker.TypeaheadListPickerConfigController",
     ['$rootScope', '$scope', function ($rootScope, $scope) {
 
-        // when save format is ready, tell it this picker is a typeahead
-        // this is currently required as this is executed before the save format controller
-        $scope.$on('saveFormatListening', function (event, arg) {
-            $rootScope.$broadcast('isTypeaheadListPicker', true);
-        });
-
     }]);
