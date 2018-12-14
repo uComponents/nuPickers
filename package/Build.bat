0cad299db9a4c2f8cf72b72f88c97eb8be25b782
Jeavon <jeavon@crumpled-dog.com>
2018-12-14 18:09:25 +0000

    Switch to using vswhere for builds

--- package/Build.bat
+++ package/Build.bat
@@ -3,4 +3,27 @@ ECHO APPVEYOR_REPO_TAG: %APPVEYOR_REPO_TAG%
 ECHO APPVEYOR_BUILD_NUMBER : %APPVEYOR_BUILD_NUMBER%
 ECHO APPVEYOR_BUILD_VERSION : %APPVEYOR_BUILD_VERSION%
 Call nuget restore ..\source\nuPickers.sln
-Call "%programfiles(x86)%\MSBuild\12.0\Bin\MsBuild.exe" package.proj
+
+SET toolsFolder=%CD%\tools\
+IF NOT EXIST "%toolsFolder%" (
+  MD tools
+)
+
+IF NOT EXIST "%toolsFolder%vswhere.exe" (
+  ECHO vswhere not found - fetching now
+  nuget install vswhere -Version 2.5.2 -Source nuget.org -OutputDirectory tools
+)
+
+FOR /f "delims=" %%A in ('dir "%toolsFolder%vswhere.*" /b') DO SET "vswhereExePath=%toolsFolder%%%A\"
+MOVE "%vswhereExePath%tools\vswhere.exe" "%toolsFolder%vswhere.exe"
+
+for /f "usebackq tokens=1* delims=: " %%i in (`"%CD%\tools\vswhere.exe" -latest -requires Microsoft.Component.MSBuild`) do (
+  if /i "%%i"=="installationPath" set InstallDir=%%j
+)
+
+SET VSWherePath="%InstallDir%\MSBuild"
+
+ECHO.
+ECHO Visual Studio is installed in: %InstallDir%
+
+CALL "%InstallDir%\MSBuild\15.0\Bin\amd64\MsBuild.exe" package.proj %~1
