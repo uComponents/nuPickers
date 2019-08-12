61d2340eab8c424b253c45e52e7981971e00fbf7
arkadiuszbiel <abi@novicell.co.uk>
2019-08-12 11:15:00 +0100

    Solve problem with embed resources

--- source/nuPickers/nuPickerComposer.cs
+++ source/nuPickers/nuPickerComposer.cs
@@ -1,20 +1,23 @@
 using nuPickers.Components;
+using nuPickers.EmbeddedResource;
 using nuPickers.Shared.DataSource;
 using nuPickers.Shared.EnumDataSource;
 using Umbraco.Core;
 using Umbraco.Core.Composing;
+using Umbraco.Web.Runtime;
 
 namespace nuPickers
 {
     [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
-
-    public class nuPickerComposer :  ICoreComposer
+    [ComposeAfter(typeof(WebFinalComposer))]
+    public class nuPickerComposer :  IComposer
     {
         public void Compose(Composition composition)
         {
+            composition.Register(factory => new EmbeddedResourceController());
+            composition.Components().Append<EmbeddedResourceCompontent>();
             composition.Register<CacheInvalidationComponent>(
                 Lifetime.Singleton);
-            composition.Register<EmbeddedResourceCompontent>(Lifetime.Singleton);
 
             composition.Register<RelationMappingComponent>(Lifetime.Singleton);
             composition.Register(typeof(DataSourceApiController), Lifetime.Request);
